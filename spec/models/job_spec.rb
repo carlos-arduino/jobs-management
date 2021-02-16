dff77d2c16aa85206791c0de6b200b0d7ad8f311
carlos-arduino <cae_arduino@icloud.com>
2021-02-16 16:38:08 -0300

    fix application layout log out

--- spec/models/job_spec.rb
+++ spec/models/job_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe Job, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Job do
+  context 'validation' do
+    it { should validate_presence_of(:title) }
+    
+    it { should validate_presence_of(:description) }
+
+    it { should validate_presence_of(:level) }
+
+    it { should validate_presence_of(:limit_date) }
+    
+    it { should validate_presence_of(:quantity) }
+  end
+
 end
