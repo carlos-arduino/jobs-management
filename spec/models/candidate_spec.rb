416a8107e1a84d9e539a21e8cae2c72a4dc5bce9
carlos-arduino <cae_arduino@icloud.com>
2021-02-26 16:39:53 -0300

    add unity tests for model Proposal and Candidate

--- spec/models/candidate_spec.rb
+++ spec/models/candidate_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe Candidate, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Candidate do
+  context 'validation' do
+    it { should validate_presence_of(:email) }
+
+    it { should validate_presence_of(:password) }
+  end
 end
--- spec/models/proposal_spec.rb
+++ spec/models/proposal_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe Proposal, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Proposal do
+  context 'validation' do
+    it { should validate_presence_of(:start_date) }
+    
+    it { should validate_presence_of(:salary_proposal) }
+
+  end
 end
